862a3f2d62c80c961e5bc013e692e33fcf7491f4
Desgard_Duan <desgard_duan@foxmail.com>
2015-10-01 17:26:31 +0800

    update 2015-10-01

--- js/test.js
+++ js/test.js
@@ -1,6 +1,7 @@
 /*
  * description: 嵌入js，重写原函数
  */
+var setTime = "2200";
 
 var js = "";
 js += 'function OutMessage() {' + 'document.getElementById("message").style.display = "";' + 'document.getElementById("block").style.display = "";' + 'run = true;' + 'cTime = 10;' + 'line = false;' + 'TimeClose();}'
@@ -15,7 +16,7 @@ js += 'function TimeClose() {' + 'var x = document.getElementById("message");' +
 
 js += 'function updateUserData() {' + 'var state_id = xmlHttp.responseXML.getElementsByTagName("select_state")[0].firstChild.nodeValue;' + 'var select_message = xmlHttp.responseXML.getElementsByTagName("select_message")[0].firstChild.nodeValue;' + 'if (state_id == "0") {' + 'alert(select_message + "，如果连续弹出该对话框说明没有计时，第一重新登录，第二请使用IE或Chrome浏览器，请勿使用360浏览器。");' + 'window.setTimeout("UpdateUserTime()", 10000);' + '} else if (state_id == "2") {' + 'alert(select_message + "，本次计时已结束，页面将关闭，如需继续观看本视频请重新打开网页");' + 'window.close();' + '} else if (state_id == "3") {' + 'console.log("本次计时结束，将会开始新的计时");' + 'window.is_upload = 0;' + 'reWriteCONST();' + 'line = true;' + 'UpdateUserTime();' + '} else {' + 'sid = select_message;' + 'is_upload = 1;' + 'document.getElementById("ranstring").value = "";' + 'GetPhotoAgain();' + 'FromUserLogin();}}'
 
-js += 'function reWriteCONST() {' + 'window.random = 450;' + 'console.log("Bingo~");}'
+js += 'function reWriteCONST() {' + 'window.random = ' + setTime + ';' + 'console.log("Bingo~");}'
 
 js += 'setTimeout(reWriteCONST,5000);' + 'console.log("冬瓜：成功载入脚本");'
 js += 'function StopVideo() {' + 'ret = true;' + 'console.log("啦啦啦，计时成功咯！(⊙o⊙)");' + 'if (ret) {' + 'is_upload = 3;' + 'random = -1;' + 'UpdateUserTime();' + '}' + '}';
